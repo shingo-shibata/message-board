5cbbc88d58dc712702078c08330aae28c8c4c874
Shingo Shibata <munjapan@hotmail.com>
2016-11-28 11:06:49 +0000

    create message model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'messages#index'
+  resources :messages , only: [:create]
+end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -56,4 +58,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
