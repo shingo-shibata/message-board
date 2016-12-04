8e4f7027db93ec4ff8cc4f881bcd3d4f5f7715b3
Shingo Shibata <munjapan@hotmail.com>
2016-12-04 05:26:16 +0000

    revised

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ActiveRecord::Base
-    validates :name, length: {maximum: 20}, presence: true
-    validates :body, length; {maximum: 2, maximum: 30}, presence: true 
+    # validates :name, length: {maximum: 20}, presence: true
+    # validates :body, length: {miniimum: 2, maximum: 30}, presence: true 
 end
