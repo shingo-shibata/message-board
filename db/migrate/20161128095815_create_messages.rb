b920ba6cad7057e968f0bd328bddc1cf9d0d1a42
Shingo Shibata <munjapan@hotmail.com>
2016-12-04 13:26:22 +0000

    update for heroku

--- db/migrate/20161128095815_create_messages.rb
+++ db/migrate/20161128095815_create_messages.rb
@@ -2,9 +2,7 @@ class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
       t.string :name
-      t.integer :age
       t.string :body
- 
       
       t.timestamps null: false
     end
