7bab385d9b4bde92594a747919e09c19a1ef81be
Shingo Shibata <munjapan@hotmail.com>
2016-12-06 08:38:42 +0000

    revised

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,5 +4,5 @@ class Message < ActiveRecord::Base
   # 内容は必須入力かつ2文字以上30文字以下
   validates :body , length: { minimum: 2, maximum: 30 } , presence: true
   # 値は0以上150以下かつ数字入力
-  validates :age , numericality: [{ greater_than: 0, less_than: 150} , true]
+  validates :age , numericality: { greater_than: 0, less_than: 150} 
 end
